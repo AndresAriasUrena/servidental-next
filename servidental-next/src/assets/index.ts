13e2b60c0e0ad0dec91ad0a7fda21f42ab6f8490
Andres Arias <andresarias@Andress-MacBook-Pro.local>
2024-10-29 01:16:04 -0600

    assets complete

--- servidental-next/src/assets/index.ts
+++ servidental-next/src/assets/index.ts
@@ -1,4 +1,3 @@
-// src/assets/index.ts
 import { StaticImageData } from 'next/image'
 
 // Logos principales
@@ -19,61 +18,173 @@ import sturdyLogo from './logos/brands/sturdy_logo.jpg'
 import tpcLogo from './logos/brands/tpc_logo.png'
 import xpectVisionLogo from './logos/brands/xpect-vision.png'
 
-// Productos destacados
-import unidadDentalS30 from './products/unidad_dental_serie_s30/serie-s30.png'
-import unidadDentalS30Carrito from './products/unidad_dental_serie_s30/serie-s30-carrito.png'
-import fresadora from './products/fresadora/fresadora-2.png'
-import rayosX from './products/rayos_x_portatil/rayos-x-portatil-4.png'
-
 // Anestesia
-import anestesiaImg from './products/anestesia/GENI2.png';
+import anestesiaImg from './products/anestesia/GENI2.png'
 
 // Bombas de vacío
-import anyvacImg from './products/bomba_de_vacio_humeda/anyvac30_1.png';
-import vc10Img from './products/bomba_de_vacio_seca/vc-10_1.png';
+import anyvacImg from './products/bomba_de_vacio_humeda/anyvac30_1.png'
+import vc10Img from './products/bomba_de_vacio_seca/vc-10_1.png'
 
 // Motores
-import endomotorImg from './products/c-smart-endomotor/endomotor.jpg';
+import endomotorImg from './products/c-smart-endomotor/endomotor.jpg'
+import cPumaImg from './products/motor_electrico_cirugia/c-puma.jpg'
+import motorElectricoCirugia from './products/motor_electrico_cirugia/motor-electrico-cirugia.png'
+import motorImplantes from './products/motor_implantes/motor-de-implantes.png'
+import sailorPro from './products/motor_implantes/sailor-pro.png'
+import nx201n1 from './products/motor_nx-210n_laboratorio/nx-201n-1.png'
+import nx201n2 from './products/motor_nx-210n_laboratorio/nx-201n-2.png'
 
 // Cámaras
-import camaraIntraoral1 from './products/camara-intraoral/camara-1.png';
-import camaraIntraoral2 from './products/camara-intraoral/camara-2.png';
+import camaraIntraoral1 from './products/camara-intraoral/camara-1.png'
+import camaraIntraoral2 from './products/camara-intraoral/camara-2.png'
 
 // Carrito
-import carrito1 from './products/carrito_multifuncional/carrito-1_1.png';
-import carrito2 from './products/carrito_multifuncional/carrito-2_1.png';
-import carrito3 from './products/carrito_multifuncional/carrito-3_1.png';
+import carrito1 from './products/carrito_multifuncional/carrito-1_1.png'
+import carrito2 from './products/carrito_multifuncional/carrito-2_1.png'
+import carrito3 from './products/carrito_multifuncional/carrito-3_1.png'
 
 // Compresores
-import compresor701 from './products/compresor/DC-701-1hp_1.png';
-import compresor702 from './products/compresor/DC-702-2hp_1.jpg';
-import compresor703 from './products/compresor/DC-703-3hp_1.jpg';
+import compresor701 from './products/compresor/DC-701-1hp_1.png'
+import compresor702 from './products/compresor/DC-702-2hp_1.jpg'
+import compresor703 from './products/compresor/DC-703-3hp_1.jpg'
 
-// Contra-ángulos
-import contraangulo1 from './products/contrangulo/contrangulo-1_1.jpg';
-import contraanguloImplantes from './products/contrangulo-implantes/contrangulo-para-implantes.png';
+// Contra-ángulos y piezas de mano
+import contraangulo1 from './products/contrangulo/contrangulo-1_1.jpg'
+import contraangulo2 from './products/contrangulo/contrangulo-2_1.jpg'
+import contraangulo3 from './products/contrangulo/contrangulo-3_1.jpg'
+import contraangulo4 from './products/contrangulo/contrangulo-4_1.jpg'
+import contraangulo5 from './products/contrangulo/contrangulo-5_1.jpg'
+import contraanguloElec from './products/contrangulo/contrangulo-elec_1.png'
+import contraanguloImplantes from './products/contrangulo-implantes/contrangulo-para-implantes.png'
+
+// Piezas de mano alta velocidad y accesorios
+import piezaMano1 from './products/pieza_de_mano_alta_velocidad/pieza-de-mano-1.png'
+import piezaMano2 from './products/pieza_de_mano_alta_velocidad/pieza-de-mano-2.png'
+import cExplorer1 from './products/piezas_de_mano/c-explorer-1.png'
+import cExplorer2 from './products/piezas_de_mano/c-explorer-2.png'
+import cExplorer3 from './products/piezas_de_mano/c-explorer-3.png'
+import cp11 from './products/piezas_de_mano/cp-1-1.png'
+import cp12 from './products/piezas_de_mano/cp-1-2.png'
+import cp132 from './products/piezas_de_mano/cp-1.3-2.jpg'
+import cx207f1 from './products/piezas_de_mano/cx207-f-1.png'
+import cx207f2 from './products/piezas_de_mano/cx207-f-2.png'
+import h75 from './products/piezas_de_mano/h75.png'
+import kit3Partes from './products/piezas_de_mano/kit-3-partes-piezas-de-mano.png'
+import ptMaster1 from './products/piezas_de_mano/pt-master-1.png'
+import ptMaster2 from './products/piezas_de_mano/pt-master-2.png'
+import ptMaster3 from './products/piezas_de_mano/pt-master-3.png'
+import ptMaster4 from './products/piezas_de_mano/pt-master-4.png'
+import titanSupertorque from './products/piezas_de_mano/titan-supertorque.png'
 
 // Cortadores
-import cortadorGutta from './products/cortador_gutta_percha_ultrasonico/rs=w_888,h_1184_1.jpg';
-import gpCutFit from './products/gp_cut_fit/rs=w_888,h_1184 (2).jpg';
+import cortadorGutta from './products/cortador_gutta_percha_ultrasonico/rs=w_888,h_1184_1.jpg'
+import gpCutFit from './products/gp_cut_fit/rs=w_888,h_1184 (2).jpg'
 
 // Scanner
-import eagleIOS from './products/escaner_portatil_eagle/eagle-ios-0_1.jpeg';
+import eagleIOS from './products/escaner_portatil_eagle/eagle-ios-0_1.jpeg'
+import freedom1 from './products/scanner/freedom-1.png'
+import meyerScanner from './products/scanner/meyer.webp'
+import scanner1 from './products/scanner/scanner-1.png'
+import scanner2 from './products/scanner/scanner-2.png'
+import scanner3 from './products/scanner/scanner-3.png'
 
 // Esterilizadores
-import esterilizadorManual from './products/esterilizador_manual/esterilizador-manual_1.png';
-import esterilizadorSemi from './products/esterilizador_semiautomatico/esterilziador-semiautomatico.png';
+import esterilizadorManual from './products/esterilizador_manual/esterilizador-manual_1.png'
+import esterilizadorSemi from './products/esterilizador_semiautomatico/esterilziador-semiautomatico.png'
 
-// Lámparas
-import lamparaL500 from './products/lamparas_dentales/L500.png';
-import lamparaV1 from './products/lamparas_dentales/v1.png';
-import lamparaV2 from './products/lamparas_dentales/v2.png';
+// Implantes
+import uaImplante from './products/implantes/UA-implante.png'
+
+// Irrigación ultrasónica
+import irrigacion1 from './products/irrigacion_ultrasonica_inalambrica/rs=w_864,h_906.jpg'
+import irrigacion2 from './products/irrigacion_ultrasonica_inalambrica/rs=w_888,h_1184 (1).jpg'
+
+// Lámparas de fotocurado
+import honor1 from './products/lampara_fotocurado_detector_caries/Honor 1.png'
+import fotocurado1 from './products/lampara_fotocurado_detector_caries/fotocurado-1.png'
+import fotocurado2 from './products/lampara_fotocurado_detector_caries/fotocurado-2.png'
+import fotocurado3 from './products/lampara_fotocurado_detector_caries/fotocurado-3.png'
+import fotocurado4 from './products/lampara_fotocurado_detector_caries/fotocurado-4.png'
+import fotocurado5 from './products/lampara_fotocurado_detector_caries/fotocurado-5.png'
+import lamparaOrtodoncia1 from './products/lampara_fotocurado_ortodoncia/lampara-ortodoncia-1.png'
+import lamparaOrtodoncia2 from './products/lampara_fotocurado_ortodoncia/lampara-ortodoncia-2.png'
+
+// Lámparas dentales
+import lamparaL500 from './products/lamparas_dentales/L500.png'
+import lamparaV1 from './products/lamparas_dentales/v1.png'
+import lamparaV2 from './products/lamparas_dentales/v2.png'
 
 // Lavadora ultrasonido
-import lavadora1 from './products/lavadora_ultrasonido/lavadora-1.png';
-import lavadora2 from './products/lavadora_ultrasonido/lavadora-2.png';
-import lavadoracesto from './products/lavadora_ultrasonido/lavadora-cesto.png';
+import lavadora1 from './products/lavadora_ultrasonido/lavadora-1.png'
+import lavadora2 from './products/lavadora_ultrasonido/lavadora-2.png'
+import lavadoraCesto from './products/lavadora_ultrasonido/lavadora-cesto.png'
 
+// Lector placas radiográficas
+import lectorPlacas from './products/lector_placas_radiograficas/lector-placas.png'
+
+// Microarenador
+import microjatolab from './products/microarenador_standard/microjato_lab.png'
+import microjatoPlusLab45 from './products/microarenador_standard/microjato_plus_45.png'
+import microjatoPlusLab90 from './products/microarenador_standard/microjato_plus_90.png'
+import microjatostd from './products/microarenador_standard/microjato_standard.png'
+import microjatostdSemGrav from './products/microarenador_standard/microjato_standard_sem_grav.png'
+
+// Rayos X
+import rayosX1 from './products/rayos_x_portatil/rayos-x-portatil-1.png'
+import rayosX2 from './products/rayos_x_portatil/rayos-x-portatil-2.png'
+import rayosX3 from './products/rayos_x_portatil/rayos-x-portatil-3.png'
+import rayosX4 from './products/rayos_x_portatil/rayos-x-portatil-4.png'
+import rayosX5 from './products/rayos_x_portatil/rayos-x-portatil-5.png'
+import rayosX6 from './products/rayos_x_portatil/rayos-x-portatil-6.png'
+import rayosX7 from './products/rayos_x_portatil/rayos-x-portatil-7.png'
+
+// Selladoras
+import selladoraSiger from './products/selladora/selladora-siger.png'
+import selladoraBiostamp from './products/selladora_biostamp_127v/seladora_01.png'
+import selladoraBioart0 from './products/selladora_smart/selladora-bioart-0.png'
+import selladoraBioart1 from './products/selladora_smart/selladora-bioart-1.png'
+import selladoraBioart2 from './products/selladora_smart/selladora-bioart-2.png'
+
+// Sensor digital y radiográfico
+import sensorDigital from './products/sensor_digital/sensor-digital.png'
+import sensorDigital2 from './products/sensor_digital/sensor-digital-2.png'
+import sensorDigital3 from './products/sensor_digital/sensor-digital-3.png'
+import sigerSensor1 from './products/sensor_radiografico/Siger-Sensor-Catalog(61m)-(1)-1.png'
+import sigerSensor3 from './products/sensor_radiografico/Siger-Sensor-Catalog(61m)-(1)-3.png'
+import sensorRadio from './products/sensor_radiografico/rs=w_888,h_1184 (3).jpg'
+
+// Termoformadoras
+import plastPress0 from './products/termoformdoras/plast-press-0.png'
+import plastPress1 from './products/termoformdoras/plast-press-1.png'
+import plastPress2 from './products/termoformdoras/plast-press-2.png'
+import plastPress3 from './products/termoformdoras/plast-press-3.png'
+import plastvacAnalogo from './products/termoformdoras/plastvac-p7-127v-analogo.png'
+import plastvacDigital0 from './products/termoformdoras/plastvac-p7-127v-digital-0.png'
+import plastvacDigital1 from './products/termoformdoras/plastvac-p7-127v-digital-1.png'
+
+// Tomógrafo
+import eagleEdge3DTele from './products/tomografo/Eagle-Edge-3D-tele-completo.png'
+import eagleEdge3D from './products/tomografo/eagle-edge-3D-tele.jpeg'
+
+// Unidades dentales
+import unidadDentalS30 from './products/unidad_dental_serie_s30/serie-s30.png'
+import unidadDentalS30Carrito from './products/unidad_dental_serie_s30/serie-s30-carrito.png'
+import unidadDentalS30Tapiceria from './products/unidad_dental_serie_s30/serie-s30-tapiceria.png'
+import unidadDentalU200 from './products/unidad_dental_u200/u-200.png'
+import unidadDentalV600 from './products/unidad_dental_v600/v-600.png'
+import unidadDentalV600Tapiceria from './products/unidad_dental_v600/v-6000-tapiceria.png'
+
+// Vaporizadores
+import ckA from './products/vaporizador_desinfeccion_1tubo/CK-A.png'
+import ckA1 from './products/vaporizador_desinfeccion_1tubo/CK-A1.png'
+import ckA2 from './products/vaporizador_desinfeccion_1tubo/CK-A2.png'
+import ckA3 from './products/vaporizador_desinfeccion_1tubo/CK-A3.png'
+import ckA4 from './products/vaporizador_desinfeccion_1tubo/CK-A4.png'
+import ckA5 from './products/vaporizador_desinfeccion_1tubo/CK-A5.png'
+import ckA6 from './products/vaporizador_desinfeccion_1tubo/CK-A6.png'
+import ckA7 from './products/vaporizador_desinfeccion_1tubo/CK-A7.png'
+import ckC from './products/vaporizador_desinfeccion_2tubo/CK-C.png'
+import ckC1 from './products/vaporizador_desinfeccion_2tubo/CK-C1.png'
 
 // Imágenes de servicios
 import instalacionesImg from './services/instalaciones.jpg'
@@ -110,7 +221,18 @@ const assets = {
       vc10: vc10Img
     },
     motores: {
-      endomotor: endomotorImg
+      endomotor: endomotorImg,
+      electrico: {
+        cPuma: cPumaImg,
+        cirugia: motorElectricoCirugia
+      },
+      implantes: {
+        default: motorImplantes,
+        sailorPro
+      },
+      laboratorio: {
+        nx201n: [nx201n1, nx201n2]
+      }
     },
     camaras: {
       intraoral: [camaraIntraoral1, camaraIntraoral2]
@@ -124,19 +246,46 @@ const assets = {
       dc703: compresor703
     },
     contraangulos: {
-      default: contraangulo1,
+      standard: [contraangulo1, contraangulo2, contraangulo3, contraangulo4, contraangulo5],
+      electrico: contraanguloElec,
       implantes: contraanguloImplantes
+    },
+    piezasMano: {
+      altaVelocidad: [piezaMano1, piezaMano2],
+      cExplorer: [cExplorer1, cExplorer2, cExplorer3],
+      cp1: [cp11, cp12, cp132],
+      cx207f: [cx207f1, cx207f2],
+      h75,
+      kit3Partes,
+      ptMaster: [ptMaster1, ptMaster2, ptMaster3, ptMaster4],
+      titanSupertorque
     },
     cortadores: {
       gutta: cortadorGutta,
-      gpCutFit: gpCutFit
+      gpCutFit
     },
     scanner: {
-      eagleIOS: eagleIOS
+      eagleIOS,
+      freedom: freedom1,
+      meyer: meyerScanner,
+      images: [scanner1, scanner2, scanner3]
     },
     esterilizadores: {
       manual: esterilizadorManual,
       semiAuto: esterilizadorSemi
+    },
+    implantes: {
+      ua: uaImplante
+    },
+    irrigacionUltrasonica: {
+      images: [irrigacion1, irrigacion2]
+    },
+    lamparasFotocurado: {
+      detector: {
+        honor: honor1,
+        images: [fotocurado1, fotocurado2, fotocurado3, fotocurado4, fotocurado5]
+      },
+      ortodoncia: [lamparaOrtodoncia1, lamparaOrtodoncia2]
     },
     lamparasDentales: {
       l500: lamparaL500,
@@ -144,19 +293,71 @@ const assets = {
       v2: lamparaV2
     },
     lavadoraUltrasonido: {
-      lava1: lavadora1,
-      lava2: lavadora2,
-      lavacesto: lavadoracesto
+      images: [lavadora1, lavadora2],
+      cesto: lavadoraCesto
     },
-    // ... mantener los productos que ya estaban ...
-    unidadDental: {
+    lectorPlacas: {
+      default: lectorPlacas
+    },
+    microarenador: {
+      lab: microjatolab,
+      plus: {
+        lab45: microjatoPlusLab45,
+        lab90: microjatoPlusLab90
+      },
+      standard: microjatostd,
+      standardSemGrav: microjatostdSemGrav
+    },
+    rayosX: {
+      portatil: [rayosX1, rayosX2, rayosX3, rayosX4, rayosX5, rayosX6, rayosX7]
+    },
+    selladoras: {
+      siger: selladoraSiger,
+      biostamp: selladoraBiostamp,
+      bioart: [selladoraBioart0, selladoraBioart1, selladoraBioart2]
+    },
+    sensores: {
+      digital: [sensorDigital, sensorDigital2, sensorDigital3],
+      radiografico: {
+        siger: [sigerSensor1, sigerSensor3],
+        default: sensorRadio
+      }
+    },
+    termoformadoras: {
+      plastPress: [plastPress0, plastPress1, plastPress2, plastPress3],
+      plastvac: {
+        analogo: plastvacAnalogo,
+        digital: [plastvacDigital0, plastvacDigital1]
+      }
+    },
+    tomografo: {
+      eagleEdge: {
+        completo: eagleEdge3DTele,
+        tele: eagleEdge3D
+      }
+    },
+    unidadesDentales: {
       s30: {
         default: unidadDentalS30,
         carrito: unidadDentalS30Carrito,
+        tapiceria: unidadDentalS30Tapiceria
+      },
+      u200: {
+        default: unidadDentalU200
+      },
+      v600: {
+        default: unidadDentalV600,
+        tapiceria: unidadDentalV600Tapiceria
       }
     },
-    fresadora,
-    rayosX,
+    vaporizadores: {
+      unTubo: {
+        ckA: [ckA, ckA1, ckA2, ckA3, ckA4, ckA5, ckA6, ckA7]
+      },
+      dosTubos: {
+        ckC: [ckC, ckC1]
+      }
+    }
   },
   services: {
     instalaciones: instalacionesImg,
